94f5c5e74db2d9295079850c57e40da7c6391b92
Thibaut Démare <fdhp_76@hotmail.com>
2016-05-03 11:51:21 +0200

    Use UTF-8 encoding

--- GamaPlugin/AnalyseNetwork/src/gama/extensions/analysenetwork/AnalyseGraphElement.java
+++ GamaPlugin/AnalyseNetwork/src/gama/extensions/analysenetwork/AnalyseGraphElement.java
@@ -18,7 +18,7 @@ public class AnalyseGraphElement {
 	@doc(value = "This operator allows to compute the Shimbel index of an agent outside a graph. It considers that the agent is connected to the network since a moving agent can access the network and move on it.",
 		comment = "The user must give the GAMA graph and the gama agent, but also the characteristics of the network that must be taken into account to compute the measure " +
 				"(lentgh and speed attribute on the network and default speed outside the network)",
-		examples = { @example("shimbel_index(my_agent, my_network, 'length', 'speed', 70°km/°s)") })
+		examples = { @example("shimbel_index(my_agent, my_network, 'length', 'speed', 70ï¿½km/ï¿½s)") })
 	public static Double shimbelIndex(final IScope scope, IAgent gama_agent, IGraph gama_graph, String length_attribute, String speed_attribute, double default_speed) {
 		// Step 1 : get the gs graph associated to the gama graph
 		Graph graph = Tools.getGraph(scope, gama_graph, length_attribute, speed_attribute);
@@ -39,7 +39,7 @@ public class AnalyseGraphElement {
 	@doc(value = "This operator allows to connect agents to a graph in order to take them into account in order to compute measures on graph.",
 		comment = "The user must give the GAMA graph and the list gama agents, but also the characteristics of the network that must be taken into account to compute the measure " +
 				"(lentgh and speed attribute on the network and default speed outside the network)",
-		examples = { @example("connect_to(my_agents, my_network, 'length', 'speed', 70°km/°s)")})
+		examples = { @example("connect_to(my_agents, my_network, 'length', 'speed', 70ï¿½km/ï¿½s)")})
 	public static boolean connectTo(final IScope scope, GamaList<IAgent> gama_agents, IGraph gama_graph, String length_attribute, String speed_attribute, double default_speed) {
 		Graph graph = Tools.getGraph(scope, gama_graph, length_attribute, speed_attribute);
 		for(IAgent gama_agent : gama_agents){
