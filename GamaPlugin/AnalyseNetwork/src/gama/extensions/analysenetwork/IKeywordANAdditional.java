77e71886f9b8838f2d849cad19276bf2240b7ad4
Thibaut Démare <fdhp_76@hotmail.com>
2017-02-16 15:57:26 +0100

    Extract the plugins and the GraphAnalyser project to put them into their own repo

--- GamaPlugin/AnalyseNetwork/src/gama/extensions/analysenetwork/IKeywordANAdditional.java
+++
@@ -1,8 +0,0 @@
-package gama.extensions.analysenetwork;
-
-import msi.gama.common.interfaces.IKeyword;
-
-public class IKeywordANAdditional  implements IKeyword{
-	public static final String SHIMBEL = "shimbel_index";
-	public static final String CONNECT_TO = "connect_to";
-}
